ae3ed4bbf450b7e6db26f3a80dec1cce310c9107
onealonee <kazimiov.sk.xx@mail.ru>
2023-10-17 12:00:06 +0300

    1

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+    <div>Кол-во лайков: <strong> {{ likes }}</strong></div>
+    <div>Кол-во disлайков: <strong> {{ dislikes }}</strong></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return{
+      likes: 0,
+      dislikes: 0,
+    }
+  },
+  methods: {
+    addLike() {
+      this.likes += 1
+    },
+    addDislike() {
+      this.dislikes += 1
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
