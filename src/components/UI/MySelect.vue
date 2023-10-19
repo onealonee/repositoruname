b758dd508e0d65663011771c4164ebff4948d4c4
onealonee <kazimiov.sk.xx@mail.ru>
2023-10-19 14:04:43 +0300

    1

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,36 @@
+<template>
+  <select v-model="modelValue" @change="changeOption">
+    <option disabled value=""> Выберите из списка</option>
+    <option
+        v-for="option in options "
+        :key="option.value"
+        :value="option.value"
+    >
+      {{ option.name}}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'my-select',
+  props: {
+    modelValue: {
+      type: String
+    },
+    options: {
+      type: Array,
+      default: () => []
+    }
+  },
+  methods: {
+    changeOption(option) {
+      this.$emit('update: modelValue', option)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
